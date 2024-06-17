7874d6cf402c9933df02366eadbed8e5214e5dde
ElisaMavilia <elisamavilia1@gmail.com>
2024-06-17 19:02:18 +0200

    fixed

--- app/Models/Hall.php
+++ app/Models/Hall.php
@@ -4,8 +4,36 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\Movie;
 class Hall extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'seats',
+        'isense',
+        'price'
+    ];
+
+    public function movies(){
+        return $this->belongsToMany(Movie::class)->using(HallMovie::class);
+    }
+    public static function generatePrice($hall){
+        $bigPrice= 10;
+        $smallPrice=8;
+        $basePrice=0;
+        $isensePrice= 3;
+        if($hall->seats > 100){
+            $basePrice=$bigPrice;
+        }
+        else{
+            $basePrice=$smallPrice;
+        }
+        $finalPrice=$basePrice+$isensePrice;
+        if(!$hall->isense){
+            $finalPrice= $basePrice;
+        }
+        return  $finalPrice;
+    } 
 }
--- database/seeders/HallSeeder.php
+++ database/seeders/HallSeeder.php
@@ -14,34 +14,22 @@ class HallSeeder extends Seeder
             [
                 'name' => 'Blu',
                 'seats' => 100,
-                'isense' => false,
-                'base_price' => 10.00,
-                'created_at' => now(),
-                'updated_at' => now(),
+                'isense' => false
             ],
             [
                 'name' => 'Verde',
                 'seats' => 120,
                 'isense' => true,
-                'base_price' => 10.00,
-                'created_at' => now(),
-                'updated_at' => now(),
             ],
             [
                 'name' => 'Gialla',
                 'seats' => 50,
-                'isense' => false,
-                'base_price' => 8.00,
-                'created_at' => now(),
-                'updated_at' => now(),
+                'isense' => false
             ],
             [
                 'name' => 'Rossa',
                 'seats' => 50,
                 'isense' => false,
-                'base_price' => 8.00,
-                'created_at' => now(),
-                'updated_at' => now(),
             ],
         ];
         foreach ($halls as $hall) {
@@ -49,7 +37,7 @@ class HallSeeder extends Seeder
             $newHall->name = $hall['name'];
             $newHall->seats = $hall['seats'];
             $newHall->isense = $hall['isense'];
-            $newHall->base_price = $hall['base_price'];
+            $newHall->price = Hall::generatePrice($newHall);
             $newHall->save();
         }
     }
