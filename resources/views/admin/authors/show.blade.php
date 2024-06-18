73f8fc75718d184205784d21e9c6933420def43e
DanieleSalinitro <daniele.salinitro@icloud.com>
2024-06-18 10:57:45 +0200

    Co-authored-by: Gianmarco Bramucci <GianmarcoBramucci@users.noreply.github.com> Co-authored-by: Elisa <ElisaMavilia@users.noreply.github.com>

--- resources/views/admin/authors/show.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.admin')
-
-@section('content')
-<div class="container">
-    <h1>{{ $author->name }}</h1>
-    <a href="{{ route('admin.authors.index') }}" class="btn btn-secondary">Back to List</a>
-</div>
-@endsection
